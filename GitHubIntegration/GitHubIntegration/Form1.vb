b36d556a42ab94c0e824b9d5cd9e0d1a4b4d3c8c
HelloPrakash1980 <prakash.patel@cimcondigital.com>
2021-09-22 10:28:36 +0530

    added new code

--- GitHubIntegration/GitHubIntegration/Form1.vb
+++ GitHubIntegration/GitHubIntegration/Form1.vb
@@ -1,5 +1,14 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("First Messsage for Git hub integration")
+        Try
+
+            Dim str As String
+
+            str = ""
+        Catch ex As Exception
+
+        End Try
+
     End Sub
 End Class
